16b53602e9ffd62ff3ad2132f0d8aeaa4b0b9b90
mehmetoguzderin <mehmetoguzderin@mehmetoguzderin.com>
2020-09-07 18:36:02 +0000

    Deploying to master from  @ d177394b0e669c1ea162fc9ff7b6978d1b67f174 🚀

---
+++ 599f30e8.6cf197e5.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{63:function(a){a.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"iztech","name":"iztech","count":1,"permalink":"/blog/tags/iztech"}')}}]);
